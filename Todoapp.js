c65121f6f9b12bd106817c2932b4614de4717f8f
Moizajmal2456 <moizajmal2456@gmail.com>
2023-04-19 10:40:26 +0500

    second commit

--- Todoapp.js
+++ Todoapp.js
@@ -5,7 +5,31 @@
 
  const userInput = document.getElementById("input");
  const selectAll = document.getElementById("select-all");
- const filters = document.getElementById("filters-wrapper");
+ const filters = document.getElementById("filters-dropdown");
+ const list = document.querySelector(".task-list");
+
+ // settaskui function
+ const setTaskItemUI = (obj) => 
+ {
+
+  const currentId = obj.id;
+   // create a new list item (li)
+   const taskItem = document.createElement("li");
+   // taskItem.classList.add("task-item")
+   taskItem.className = "task-item";
+   taskItem.id = `item_${currentId}`;
+   // inner text set
+   taskItem.innerHTML = `<div class="checkbox" onclick="markAsComplete(${currentId})"></div>
+     <span>${obj.title}
+     </span>
+     <button id="edit_${currentId}" onclick="updateTask(${currentId})">Edit</button>
+     <button onclick="deleteTask(${currentId})">Delete</button>`;
+ 
+   //access list
+  // const list = document.querySelector(".task-list");
+   // append the new li to existing ul
+   list.appendChild(taskItem);
+ };
 
  const keyPressListener = (event) => {
  if (event.key === "Enter"){
@@ -47,23 +71,7 @@
 
  tasksList.push(obj);
 
-  const currentId = obj.id;
-   // create a new list item (li)
-   const taskItem = document.createElement("li");
-   // taskItem.classList.add("task-item")
-   taskItem.className = "task-item";
-   taskItem.id = `item_${currentId}`;
-   // inner text set
-   taskItem.innerHTML = `<div class="checkbox" onclick="markAsComplete(${currentId})"></div>
-     <span>${obj.title}
-     </span>
-     <button id="edit_${currentId}" onclick="updateTask(${currentId})">Edit</button>
-     <button onclick="deleteTask(${currentId})">Delete</button>`;
- 
-   //access list
-   const list = document.querySelector(".task-list");
-   // append the new li to existing ul
-   list.appendChild(taskItem);
+ setTaskItemUI(obj);
 
  resetInputField();
  };
@@ -86,6 +94,23 @@
  currentTaskItem.classList.toggle("completed");
 
  };
+
+ //filters completed task function
+ const markItemsAsComplete = (id) => {
+ const activeCheckbox = document.querySelector(`#item_${id} .checkbox`);
+ const currentTaskItem = document.querySelector(`#item_${id}`);
+
+ activeCheckbox.classList.add("checkbox-filled");
+ currentTaskItem.classList.add("completed");
+ };
+
+ const markItemsAsInComplete = (id) => {
+  const activeCheckbox = document.querySelector(`#item_${id} .checkbox`);
+  const currentTaskItem = document.querySelector(`#item_${id}`);
+
+  activeCheckbox.classList.remove("checkbox-filled");
+  currentTaskItem.classList.remove("completed");
+};
 
  //update task  function
  const updateText = (tasktext) => {
@@ -132,7 +157,6 @@
       ? currentTaskItem.classList.add("completed")
       : currentTaskItem.classList.remove("completed");
    })
-
  } 
 
  //event handling of markedad complete checkbox
@@ -140,21 +164,57 @@
 
  //Filters change function
  const handleFiltersChange = (event) => {
+ console.log("i am in event change");
  const { value }  = event.target;
 
  //switch case on all | pending | completed
  switch(value){
-   case"all":
-   
+   case"All":
+   console.log("i am in all filter");
+   list.innerHTML = " ";
+   tasksList.forEach((t) => {
+    setTaskItemUI(t);
+    if(t.isComplete)
+    {
+      markItemsAsComplete(t.id);
+    }
+    else {
+      markItemsAsInComplete(t.id);
+    }
+   });
+   break;
 
+   case"Completed":
+   console.log("i am in completed filter");
+   const completeTask = tasksList.filter((t) => t.isComplete === true);
+   list.innerHTML = " ";
+   completeTask.forEach((t) => {
+   setTaskItemUI(t);
+   markItemsAsComplete(t.id);
+   });
+   
+   if (completeTask.length === 0) {
+    list.innerHTML = "<p><i>There are no completed tasks</i><p>";
+  }
+ break;
+
+ case"Pending":
+ console.log("i am in pending filter");
+ const pendingTasks = tasksList.filter((t) => t.isComplete === false);
+ list.innerHTML = "";
+
+ pendingTasks.forEach((t) => {
+   setTaskItemUI(t);
+   markItemsAsInComplete(t.id);
+ });
+ break;
+
+ default:
+ console.log("All");
+ break;
  }
  };
 
  //event handling of selectors function
  filters.addEventListener("change" , handleFiltersChange );
 
-
- // settaskui function
- const setTaskItemUI = (obj) => 
- {
- };
